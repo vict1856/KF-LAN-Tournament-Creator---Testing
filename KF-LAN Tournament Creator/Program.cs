de2bc92476c7d52f638a6ec080e5263a3c2d4bdf
Victor Heine Holten <vict1856@edu.eal.dk>
2018-11-29 12:26:49 +0100

    Add project files.

---
+++ KF-LAN Tournament Creator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KF_LAN_Tournament_Creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
